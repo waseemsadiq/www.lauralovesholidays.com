84723c14c89b989bad22b923f9cfd2e0304819f7
waseemsadiq <waseem2202@gmail.com>
2025-02-19 16:40:58 +0000

    Retry fix
    last one didn’t work

--- vite.config.ts
+++ vite.config.ts
@@ -14,7 +14,7 @@ export default defineConfig(({ mode }) => ({
     mode === 'development' &&
     componentTagger(),
   ].filter(Boolean),
-  base: '/www.lauralovesholidays.com/',
+  base: '/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
